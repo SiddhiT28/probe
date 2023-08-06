97dffc62a12a2640ace32cf3abcfb9e13ba0d78a
SiddhiT28~ <forfuture1028@gmail.com>
2023-08-07 01:11:22 +0530

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer = () => {
   return (
     <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200"> 
-      <h1>2021 Google, Inc.</h1>
+      <h1>2023 Probe🔍.</h1>
     </div>
   );
 };
